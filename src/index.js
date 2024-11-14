6200e9543d678c76836a2f72396e064c3cd7ee82
mostasma <138394133+mostasma@users.noreply.github.com>
2024-11-14 19:01:18 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// For React 17 or lower
+import React from "react";
+import ReactDOM from "react-dom"; // For React 17 and earlier
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);

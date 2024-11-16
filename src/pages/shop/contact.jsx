fb97da24b63237f046d122521d02c35c9bc12be0
mostasma <138394133+mostasma@users.noreply.github.com>
2024-11-16 12:02:44 +0100

    Add files via upload

--- src/pages/shop/contact.jsx
+++ src/pages/shop/contact.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import "./contact.css";
 
 export const Contact = () => {
-  return <div>contact page</div>;
+ 
+return (
+    <section id="contact" className="contact">
+      <h2>Contact with us</h2>
+      <p>Email: asmahasan35@gmail.com</p>
+      <p>Email:milikhan.fi@gmai.com</p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/feed/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+      <p>Facebook: <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a></p>
+    </section>
+  );
 };
 export default Contact;
